6bf4ebc58aec1be021cd3f1e64a624beacb2e7e2
Diffen <diffen@me.com>
2020-08-09 12:57:07 +0200

    couple of changes

--- basic.py
+++ basic.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
 	return render_template('basic.html')
 
+@app.route('/discord')
+def discord():
+	return render_template('discord.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
